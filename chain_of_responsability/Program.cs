247b2eb787bf4bddbac8050622425e15ca36a70d
Maxime Cohen <maxime.cohen2@epsi.fr>
2018-05-14 11:02:52 +0200

    Ajout du projet chain of responsability

---
+++ chain_of_responsability/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chain_of_responsability
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
